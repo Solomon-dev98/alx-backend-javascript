62e50ece78fe14475d6e1a3761a08a2363db74c6
Solomon-dev98 <asapnino01@gmail.com>
2024-11-27 15:29:11 +0100

    initial commit

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message to STDOUT
+ * Executing a basic javascript with Node Js
+ */
+
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+module.exports = displayMessage;

efc50cd326d3a12833223b1690bf02dbf4aae522
Solomon-dev98 <asapnino01@gmail.com>
2024-11-28 10:34:33 +0100

    add scripts for project NodeJs basics

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,10 @@
 /**
- * Displays a message to STDOUT
- * Executing a basic javascript with Node Js
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
  */
-
-const displayMessage = (message) => {
-  console.log(message);
+const displayMessage = (msg) => {
+  console.log(msg);
 };
 
 module.exports = displayMessage;
